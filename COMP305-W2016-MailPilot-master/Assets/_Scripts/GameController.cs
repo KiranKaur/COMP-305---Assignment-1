92c88ff34842c68dc053ad909aa47891c5eb3259
KiranKaur <k.angel117@yahoo.com>
2016-01-28 13:26:58 -0500

    start up

---
+++ COMP305-W2016-MailPilot-master/Assets/_Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    //  public instance variable
+    public int cloudNumber=3;
+    public CloudController cloud;
+
+	// Use this for initialization
+	void Start () {
+
+        this._intialize();
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    //PRIVATE METHODS
+    //INTIALIZE METHOD
+    private void _intialize()
+    {
+       for(int cloudCount = 0; cloudCount < this.cloudNumber; cloudCount++)
+        {
+            Instantiate(cloud.gameObject);
+        }
+
+    }
+}
---
+++ COMP305-W2016-MailPilot-master/Assets/_Scripts/IslandController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IslandController : MonoBehaviour {
+	// PUBLIC INSTANCE VARIABLES
+	public float speed = 5f;
+
+	//PRIVATE INSTANCE VARIABLES
+	private Transform _transform;
+	private Vector2 _currentPosition;
+
+	// Use this for initialization
+	void Start () {
+		// Make a reference with the Transform Component
+		this._transform = gameObject.GetComponent<Transform> ();
+
+		// Reset the Island Sprite to the Top
+		this.Reset ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this._currentPosition = this._transform.position;
+		this._currentPosition -= new Vector2(this.speed,0);
+		this._transform.position = this._currentPosition;
+
+		if (this._currentPosition.x <= -470) {
+			this.Reset ();
+		}
+	}
+
+	public void Reset() {
+		float yPosition = Random.Range (-290f, 290f);
+		this._transform.position = new Vector2 (460f,yPosition);
+	}
+}
